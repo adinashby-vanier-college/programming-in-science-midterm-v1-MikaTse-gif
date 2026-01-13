6f871b729e24137f1da9bc9c053167a4864f663f
MikaTse-gif <1152shiro@gmail.com>
2026-01-13 11:01:32 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -2,34 +2,44 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    # area = radius * 2 * math.pi r nizilyou
 
+    return ""
+    # return round(area, 2)
+# print(area_of_circle(5))
+
+# *
+# **
+# * *
+# *  *
+# *****
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    result = ""
+    
+    if n < 4:
+        result += "The triangle height should be at least 4."
+
+    else:
+
+        for i in range(n - 1):
+            for j in range(1):
+                result += "*"
+
+            for k in range(i - 1):
+                result += " "
+
+            if i > 0:
+                result += "*"
+
+            result += "\n"
+
+        for i in range(n):
+            result += "*"
+
+    return result.rstrip()
+print(hollow_right_triangle(4))
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
     return ""
-
-# ----------------------------------------------------------------
-print(area_of_circle(5))
-print()
-
-print(hollow_right_triangle(3))
-print()
-
-print(hollow_right_triangle(4))
-print()
-
-print(hollow_right_triangle(5))
-print()
-
-print(inverted_pyramid(3))
-print()
-
-print(inverted_pyramid(4))
-print()
-
-print(inverted_pyramid(5))
-print()
